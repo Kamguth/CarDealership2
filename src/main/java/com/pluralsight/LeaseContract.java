485f5dcffd1f778804d55d119234f89fd195ae14
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-05-16 13:38:08 -0400

    Lease Contract complete

---
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+public class LeaseContract extends Contract {
+    private double price;
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        super(date, customerName, customerEmail, vehicleSold);
+
+        this.price = vehicleSold.getPrice();
+        this.expectedEndingValue = price * 0.5;
+        this.leaseFee = price * 0.07;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        double interestRate = 0.04;
+        return (getTotalPrice() * (1 + interestRate)) / 36 ;
+    }
+}
---
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,60 @@
+package com.pluralsight;
+
+public class SalesContract extends Contract{
+    private boolean finance;
+    private double salesTax;
+    private double recordingFee;
+    private double processingFee;
+    private double price;
+
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.finance = finance;
+        this.price = vehicleSold.getPrice();
+        this.salesTax = price * 0.05;
+        this.recordingFee = 100;
+        this.processingFee =(price < 10000) ? 295 : 495; //Ternary operator
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public double getSalesTax() {
+        return salesTax;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return price + salesTax + recordingFee + processingFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        if (!finance) return 0;
+        double totalPrice = getTotalPrice();
+        int months;
+        double rate;
+        if (price >= 10000) {
+            months = 48;
+            rate = 0.0425;
+        }
+        else {
+            months = 24;
+            rate = 0.0525;
+        }
+        return  totalPrice * (1 + rate) / months;
+    }
+}
